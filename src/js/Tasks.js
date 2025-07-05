3584713847f78e027c1e27e7a78a3a787ae08546
Tatiana Bazuto <tbazuto@gmail.com>
2025-07-05 20:13:36 +0300

    init

---
+++ src/js/Tasks.js
@@ -0,0 +1,15 @@
+export default function initData() {
+  return {
+    todo: [
+      "Welcome to Task Manager!",
+      "This is a card",
+      "Click on the button below to add a new task.",
+    ],
+    inProgress: [
+      "Make as many tasks as you need!",
+      "As I was saying, this problem here will take some of you many months to solve",
+      "Finished with a task? Drop it in Done",
+    ],
+    done: ["Well done!"],
+  };
+}
